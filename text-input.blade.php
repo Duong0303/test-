0bab98416c0d4f7881ca2a813f10582f5a5c5518
Duong <duongto010203@gmail.com>
2023-03-05 00:07:39 +0700

    text

--- text-input.blade.php
+++ text-input.blade.php
@@ -1,3 +1,15 @@
 @props(['disabled' => false])
 
 <input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) !!}>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>hdislahdkndn</h1>
+</body>
+</html>
